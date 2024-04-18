7d6c031a89ea6c6e9b81393d70cc96c1e3f6df58
nick134 <76399455+nick134-bit@users.noreply.github.com>
2024-04-18 23:34:58 +0200

    fix: isNative

--- util/isNative.ts
+++ util/isNative.ts
@@ -1,1 +1,1 @@
-export const isNativeToken = (token?: string): boolean => (token ? token.startsWith('u') || token.includes('factory' || 'ibc') : false)
+export const isNativeToken = (token?: string): boolean => (token ? token.startsWith('u') || token.includes('factory') || token.includes('ibc/') : false)
