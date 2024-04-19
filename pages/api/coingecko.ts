fff5b493fa9675d1b6a2b4cec71d0c62781a68a0
worrex <68468180+worrex@users.noreply.github.com>
2024-04-19 17:56:15 +0200

    adding gAsh and MOAR price data

--- pages/api/coingecko.ts
+++ pages/api/coingecko.ts
@@ -2,8 +2,20 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 // eslint-disable-next-line no-unused-vars
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-  const response = await fetch('https://fd60qhijvtes7do71ou6moc14s.ingress.pcgameservers.com/api/prices')
-  res.status(200).json(await response.json())
+  const pricesResponse = await fetch('https://fd60qhijvtes7do71ou6moc14s.ingress.pcgameservers.com/api/prices')
+  const guppyWhalePoolResponse = await fetch('https://ww-migaloo-rest.polkachu.com/cosmwasm/wasm/v1/contract/migaloo14p3r422qp04p345mnqe5umjy3vqx75hpxf54f8enf59wf27fksvqltavjp/smart/eyJwb29sIjp7fX0=')
+  if (!pricesResponse.ok) {
+    throw new Error(`Failed to fetch prices: ${pricesResponse.status}`)
+  }
+  if (!guppyWhalePoolResponse.ok) {
+    throw new Error(`Failed to fetch guppy whale pool: ${guppyWhalePoolResponse.status}`)
+  }
+  const guppyWhalePool = await guppyWhalePoolResponse.json()
+  const [guppy, whale] = guppyWhalePool?.data?.assets || []
+  const prices = (await pricesResponse.json()).data
+  const whalePrice = prices['white-whale']?.usd ?? 0
+  prices.guppy = { usd: Number(((Number(whale?.amount ?? 0)) / (Number(guppy?.amount ?? 0))) * (Number(whalePrice) ?? 0)).toFixed(10) }
+  res.status(200).json({ data: prices })
 }
 
 export default handler
